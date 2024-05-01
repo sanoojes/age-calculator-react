e8d865eff7342436093fea89020d97e159528da4
Sanooj Es <sanoojes6371@gmail.com>
2024-05-01 13:07:03 +0530

    chore(config): update base URI

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/age-calculator-react/",
   plugins: [react()],
   resolve: {
     alias: {
